1df8db227231fb940d1f93541345bc36b44ae2a8
Isabella <bellamay54.im@gmail.com>
2021-12-08 14:59:40 -0300

    installing dependencies

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,8 @@
+const SignIn = () => {
+    return (
+        <div>
+            <h1>entrar</h1>
+        </div>
+    )
+}
+export default SignIn;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,8 @@
+const SignUp = () => {
+    return (
+        <div>
+            <h1>cadastro</h1>
+        </div>
+    )
+}
+export default SignUp;
