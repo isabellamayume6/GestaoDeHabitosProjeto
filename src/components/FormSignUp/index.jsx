908276b9b95ceb571747ed39d829e1ba1e3a101b
Isabella <bellamay54.im@gmail.com>
2021-12-08 17:41:25 -0300

    feature:SignUp verification

--- src/components/FormSignUp/index.jsx
+++ src/components/FormSignUp/index.jsx
@@ -1,29 +1,42 @@
 import Api from '../../services/Api'
-//import * as yup from 'yup';
+import * as yup from 'yup';
 import { useForm } from "react-hook-form";
-//import { yupResolver } from '@hookform/resolvers/yup';
-//import { useHistory, Redirect } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useHistory } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
 
 const FormSignUp = () => {
-    const { register, handleSubmit } = useForm()
+    const schema = yup.object().shape({
+        username: yup.string().required('Campo obrigatório!'),
+        email: yup.string().email('e-mail inválido!').required('Campo obrigatório!'),
+        password: yup.string().required('Senha obrigatória!')
+    })
+    const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver(schema)
+    })
+    const history = useHistory()
+    const { errors } = formState
     const onSubmit = (data) => {
         Api.post("/users/", data)
             .then((reponse) => {
                 console.log(reponse)
-                toast.success('ok bro')
+                toast.success('Cadastro feito com sucesso!')
+                history.push('/')
             })
             .catch((err) => {
                 console.log(err, 'aqui errado')
-                toast.error('X')
+                toast.error('Tente novamente mais tarde.')
             })
         console.log(data)
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <h1>Cadastre-se</h1>
+            <span>{errors.username?.message}</span>
             <input placeholder='Nome' {...register('username')} />
+            <span>{errors.email?.message}</span>
             <input placeholder='e-mail' {...register('email')} />
+            <span>{errors.password?.message}</span>
             <input placeholder='senha' type='password' {...register('password')} />
             <input placeholder='confirme senha' type='password' />
             <button type='submit'>Conectar</button>
