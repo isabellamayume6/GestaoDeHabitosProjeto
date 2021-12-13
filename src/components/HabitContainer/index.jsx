49c495db37454f5a394fba43c571f6fe2f57c3a8
Guilherme Lima Desoler <guilherme.desoler@gmail.com>
2021-12-13 11:39:53 -0300

    Feature: creating habitscomponent, HabitContainer, HabitsCard.

---
+++ src/components/HabitContainer/index.jsx
@@ -0,0 +1,12 @@
+import HabitsCard from "../HabitsCard";
+import "./style.css";
+
+const HabitContainer = () => {
+  <div className="habits">
+    <h2>Hábitos</h2>
+    {/* incluir map com os hábitos
+    <HabitsCard /> */}
+  </div>;
+};
+
+export default HabitContainer;
