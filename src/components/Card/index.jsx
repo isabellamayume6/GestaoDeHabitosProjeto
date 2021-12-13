f5a855649c907776f1c9113794257c3113b0f29b
Rodrigo de Souza <souzabr@tuta.io>
2021-12-13 20:52:05 -0300

    feature(style): add style to card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,9 +4,19 @@ import {
   RiCalendarEventFill,
   RiCheckboxCircleFill,
 } from "react-icons/ri";
+
+import {
+  Container,
+  Content,
+  Footer,
+  Header,
+  AvatarContainer,
+  Pill,
+} from "./card.style";
+
 const url = `https://avatars.dicebear.com/api/big-smile/`;
-export default function Card({ isGroup = false, info, onClick: func }) {
-  console.log(info);
+
+export default function Card({ secondary = false, isGroup = false, info }) {
   let title = info.title;
   let users;
   if (isGroup) {
@@ -14,46 +24,64 @@ export default function Card({ isGroup = false, info, onClick: func }) {
     title = info.name;
   }
   return (
-    <div>
-      <img src={info.icon} alt="" />
-      <div>
-        <div>
-          <h3>{title}</h3>
-          <span className="category">{info.category}</span>
-          <span className="difficulty">{info.difficulty}</span>
-        </div>
+    <Container className="card" secondary={secondary}>
+      <img
+        className="card__icon"
+        src="https://unsplash.it/640/640
+"
+        alt=""
+      />
+      <Content className="card__content">
+        <Header className="content__header">
+          <h3 className="title">{title}</h3>
+          <div className="pills">
+            {!!info.category && (
+              <Pill isCat className="category">
+                {info.category}
+              </Pill>
+            )}
+            {!!info.difficulty && (
+              <Pill className="difficulty">{info.difficulty}</Pill>
+            )}
+          </div>
+        </Header>
         {isGroup ? (
-          <div>
-            <div>
+          <Footer>
+            <div className="details">
               <RiTodoFill />
               <span>{info.activities.length} </span>
             </div>
-            <div>
+
+            <div className="details">
               <RiFocus2Line />
               <span>{info.goals.length} </span>
             </div>
-            <div>
+
+            <AvatarContainer>
               {users.map((user) => (
-                <img key={user.id} src={`${url}${user.username}.svg`} alt="" />
+                <span className="avatar" key={user.id}>
+                  <img src={`${url}${user.username}.svg`} alt="" />
+                </span>
               ))}
-              {info.users_on_group > 4 && (
-                <span>+{info.users_on_group.length - 4}</span>
+              {info.users_on_group.length > users.length && (
+                <span>+{info.users_on_group.length - users.length}</span>
               )}
-            </div>
-          </div>
+            </AvatarContainer>
+          </Footer>
         ) : (
-          <div>
-            <div>
+          <Footer>
+            <div className="details">
               <RiCalendarEventFill />
               <span>{info.frequency}</span>
             </div>
-            <div>
+
+            <div className="details">
               <RiCheckboxCircleFill />
               <span>{info.how_much_achieved}</span>
             </div>
-          </div>
+          </Footer>
         )}
-      </div>
-    </div>
+      </Content>
+    </Container>
   );
 }
